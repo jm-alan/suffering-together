2a9b6c2b433edf932447a88084f275a60a4479c4
JM <j.maxwell.alan@gmail.com>
2022-09-16 14:43:08 -0400

    consolidate UX to single file

---
+++ frontend/src/store/UX.js
@@ -0,0 +1,81 @@
+export const SHOW_MODAL = 'UX/SHOW_MODAL';
+export const HIDE_MODAL = 'UX/SHOW_MODAL';
+export const SET_MODAL = 'UX/SET_MODAL';
+export const CLEAR_MODAL = 'UX/CLEAR_MODAL';
+export const SET_MOORING = 'UX/SET_MOORING';
+export const LOCK_LOADING = 'UX/LOCK_LOADING';
+export const UNLOCK_LOADING = 'UX/UNLOCK_LOADING';
+
+export const showModal = () => ({
+  type: SHOW_MODAL
+});
+
+export const hideModal = () => ({
+  type: HIDE_MODAL
+});
+
+export const setModal = currentModal => ({
+  type: SET_MODAL,
+  currentModal
+});
+
+export const clearModal = () => ({
+  type: CLEAR_MODAL
+});
+
+export const setMooring = modalMooring => ({
+  type: SET_MOORING,
+  modalMooring
+});
+
+export const lockLoading = () => ({
+  type: LOCK_LOADING
+});
+
+export const unlockLoading = () => ({
+  type: UNLOCK_LOADING
+});
+
+export default function reducer (
+  state = {
+    showModal: false,
+    currentModal: null,
+    modalMooring: null,
+    loadingLock: 0
+  },
+  {
+    type,
+    currentModal,
+    modalMooring
+  }
+) {
+  switch (type) {
+    case SHOW_MODAL:
+      return {
+        ...state,
+        showModal: true
+      };
+    case HIDE_MODAL:
+      return {
+        ...state,
+        showModal: false
+      };
+    case SET_MODAL:
+      return {
+        ...state,
+        currentModal
+      };
+    case CLEAR_MODAL:
+      return {
+        ...state,
+        currentModal: null
+      };
+    case SET_MOORING:
+      return {
+        ...state,
+        modalMooring
+      };
+    default:
+      return state;
+  }
+}
