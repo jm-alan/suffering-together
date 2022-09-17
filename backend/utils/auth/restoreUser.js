2c08fea643d24f3ec28118ea3420252a473625e5
JM <j.maxwell.alan@gmail.com>
2022-09-16 20:28:46 -0400

    preferential formatting

--- backend/utils/auth/restoreUser.js
+++ backend/utils/auth/restoreUser.js
@@ -1,12 +1,12 @@
 const { verify } = require('jsonwebtoken');
-const { jwtConfig } = require('../../config/server');
+const { jwtConfig: { secret } } = require('../../config/server');
 const { User } = require('../../db/models');
 
 module.exports = async (req, res, next) => {
   const { cookies: { token } } = req;
 
   try {
-    const { userID } = verify(token, jwtConfig.secret);
+    const { userID } = verify(token, secret);
     req.user = await User.findByPk(userID);
     req.user ?? res.clearCookie('token');
     next();
