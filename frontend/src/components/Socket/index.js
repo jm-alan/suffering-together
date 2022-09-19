6711679a39dcbd6d7bda490278433b398fd762f7
JM <j.maxwell.alan@gmail.com>
2022-09-18 21:22:31 -0400

    don't mount socket if no user

--- frontend/src/components/Socket/index.js
+++ frontend/src/components/Socket/index.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import getApp from '../../utils/getApp';
 
@@ -8,15 +8,18 @@ export const SocketContext = createContext();
 export default function Socket ({ children }) {
   const dispatch = useDispatch();
 
+  const user = useSelector(state => state.session.user);
+
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
+    if (!user) return;
     const ws = new window.WebSocket(getApp('ws'));
     setSocket(ws);
     return () => ws.close();
-  }, [dispatch, setSocket]);
+  }, [dispatch, setSocket, user]);
 
-  return socket && (
+  return user && socket && (
     <SocketContext.Provider value={{ socket }}>
       {children}
     </SocketContext.Provider>
