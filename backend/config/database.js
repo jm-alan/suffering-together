63ad7408064483edc83a0e22d82f212a29290226
JM <j.maxwell.alan@gmail.com>
2022-09-19 20:10:33 -0400

    tighten db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,30 +1,5 @@
-const {
-  db: {
-    username,
-    password,
-    database,
-    host
-  },
-  environment
-} = require('./server');
-
-const dialect = 'postgres';
-const seederStorage = 'sequelize';
-
 module.exports = {
-  development: {
-    logging: false,
-    username,
-    password,
-    database,
-    host,
-    dialect,
-    seederStorage
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    logging: false,
-    dialect,
-    seederStorage
-  }
-}[environment];
+  logging: false,
+  dialect: 'postgres',
+  seederStorage: 'sequelize'
+};
