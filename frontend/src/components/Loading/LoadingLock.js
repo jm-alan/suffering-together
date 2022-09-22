9104ce1c72179cd8f5ca2a5150b7b75606c58b94
JM <j.maxwell.alan@gmail.com>
2022-09-22 00:10:33 -0400

    update loading lock with new lock/unlock api

--- frontend/src/components/Loading/LoadingLock.js
+++ frontend/src/components/Loading/LoadingLock.js
@@ -3,21 +3,21 @@ import { useDispatch } from 'react-redux';
 
 import { lockLoading, unlockLoading } from '../../store/UX';
 
-export default function LoadingLock ({ name }) {
+export default function LoadingLock ({ name, children }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'development') {
       console.log('Loading lock added by', name);
     }
-    dispatch(lockLoading());
+    dispatch(lockLoading(name, children));
     return () => {
       if (process.env.NODE_ENV === 'development') {
         console.log('Loading lock released by', name);
       }
-      dispatch(unlockLoading());
+      dispatch(unlockLoading(name));
     };
-  }, []);
+  }, [dispatch, name, children]);
 
   return null;
 }
