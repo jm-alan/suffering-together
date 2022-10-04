474de99335045e64fcb9518b92b8682fe1887031
JM <j.maxwell.alan@gmail.com>
2022-10-04 01:30:43 -0400

    create sliding house container for individual house selection

--- frontend/src/components/Houses/index.js
+++ frontend/src/components/Houses/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 import FloatingPlusButton from '../FloatingPlusButton';
 import LoadingLock from '../Loading/LoadingLock';
@@ -9,13 +9,17 @@ import { getAllhouses } from '../../store/houses';
 import { setModal, showModal } from '../../store/UX';
 
 import './houses.css';
+import HouseEntry from './HouseEntry';
 
 export default function Houses () {
   const dispatch = useDispatch();
+  const { houseID } = useParams();
+  console.log(houseID);
+  console.log(!!houseID.match(/^[0-9(a-f|A-F)]{8}-[0-9(a-f|A-F)]{4}-4[0-9(a-f|A-F)]{3}-[89ab][0-9(a-f|A-F)]{3}-[0-9(a-f|A-F)]{12}$/));
 
   const user = useSelector(state => state.session.user);
   const sessionLoaded = useSelector(state => state.session.loaded);
-  const loaded = useSelector(state => state.houses.loaded);
+  const housesLoaded = useSelector(state => state.houses.loaded);
   const houses = useSelector(state => state.houses.all);
 
   const popNewHouse = () => {
@@ -31,15 +35,24 @@ export default function Houses () {
     return <Navigate to='/login' />;
   }
 
-  return loaded
+  return housesLoaded
     ? (
       <>
         <FloatingPlusButton onClick={popNewHouse} />
-        {Object.values(houses).filter($ => $).map(house => (
-          <div className='house-entry' key={house.id}>
-            {house.name}
+        <div id='residence-scroll-control'>
+          <div
+            id='residence-sliding-container'
+          >
+            <div className='residence-subcontainer left'>
+              {Object.values(houses).filter($ => $).map(house => (
+                <HouseEntry key={house.id} house={house} />
+              ))}
+            </div>
+            <div className='residence-subcontainer right'>
+              <h1>Placeholder right</h1>
+            </div>
           </div>
-        ))}
+        </div>
       </>
       )
     : <LoadingLock name='houses list' />;
