bed1e4321d331268f68d1e14e6b74c4307f1c572
JM <j.maxwell.alan@gmail.com>
2022-10-04 23:37:29 -0400

    consolidate groupings from UUID pattern because express path-to-params is a dumb bitch

--- frontend/src/utils/UUIDpattern.js
+++ frontend/src/utils/UUIDpattern.js
@@ -1,1 +1,1 @@
-export default /^[0-9(a-f|A-F)]{8}-[0-9(a-f|A-F)]{4}-4[0-9(a-f|A-F)]{3}-[89ab][0-9(a-f|A-F)]{3}-[0-9(a-f|A-F)]{12}$/;
+export default /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
