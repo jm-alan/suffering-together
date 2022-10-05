7f922bef594e55d4c203f2c6983ce9740f6ac189
JM <j.maxwell.alan@gmail.com>
2022-10-04 22:16:35 -0400

    only show plus if both enabled and shown

--- frontend/src/components/FloatingPlusButton/index.js
+++ frontend/src/components/FloatingPlusButton/index.js
@@ -4,11 +4,12 @@ import { useSelector } from 'react-redux';
 import './plus.css';
 
 export default function FloatingPlusButton ({ onClick }) {
+  const enablePlus = useSelector(state => state.UX.enablePlus);
   const showPlus = useSelector(state => state.UX.showPlus);
 
   return (
     <button
-      className={`new-item-floater${showPlus ? ' show' : ' hide'}`}
+      className={`new-item-floater${(enablePlus && showPlus) ? ' show' : ' hide'}`}
       onClick={onClick}
     >
       <span className='material-symbols-outlined'>
