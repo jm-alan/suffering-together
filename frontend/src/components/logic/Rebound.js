54307a0c3578e9b2a6af62e50fb1244f01a8acb8
JM <j.maxwell.alan@gmail.com>
2022-10-07 03:13:39 -0400

    add dev logging to Rebound component

--- frontend/src/components/logic/Rebound.js
+++ frontend/src/components/logic/Rebound.js
@@ -7,14 +7,17 @@ import {
   setReboundDestination,
   setReboundOrigin
 } from '../../store/rebound';
+import { devlog } from '../../utils/logging';
 
 export default function Rebound ({ from: startingLocation, to: finalLocation }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    devlog(`Rebound from ${startingLocation} to ${finalLocation} mounting`);
     dispatch(setReboundOrigin(startingLocation));
     dispatch(setReboundDestination(finalLocation));
     return () => {
+      devlog(`Rebound from ${startingLocation} to ${finalLocation} unmounting`);
       dispatch(clearReboundOrigin());
       dispatch(clearReboundDestination());
     };
