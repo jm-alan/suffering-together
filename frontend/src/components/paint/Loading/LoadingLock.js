9a29aa3794661fcaac898da4d2ed52c028656503
JM <j.maxwell.alan@gmail.com>
2022-10-07 03:14:20 -0400

    replace all console logs with devlog function

--- frontend/src/components/paint/Loading/LoadingLock.js
+++ frontend/src/components/paint/Loading/LoadingLock.js
@@ -1,20 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
+import { devlog } from '../../../utils/logging';
 import { lockLoading, unlockLoading } from '../../../store/UX';
 
 export default function LoadingLock ({ name, children }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (process.env.NODE_ENV === 'development') {
-      console.log('Loading lock added by', name);
-    }
+    devlog('Loading lock added by', name);
     dispatch(lockLoading(name, children));
     return () => {
-      if (process.env.NODE_ENV === 'development') {
-        console.log('Loading lock released by', name);
-      }
+      devlog('Loading lock released by', name);
       dispatch(unlockLoading(name));
     };
   }, [dispatch, name, children]);
