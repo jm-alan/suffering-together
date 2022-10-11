0c2e11d9742999c026b2c6d9c80204cfeebf50c3
JM <j.maxwell.alan@gmail.com>
2022-10-11 16:40:20 -0400

    create dev console.warn wrapper

--- frontend/src/utils/logging.js
+++ frontend/src/utils/logging.js
@@ -4,6 +4,12 @@ export const devlog = (...args) => {
   }
 };
 
+export const devwarn = (...args) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.warn(...args);
+  }
+};
+
 export const deverr = (...args) => {
   if (process.env.NODE_ENV === 'development') {
     console.error(...args);
