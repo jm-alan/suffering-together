c7513739eb691134dcf5f22de28b596e911f8230
JM <j.maxwell.alan@gmail.com>
2022-12-26 03:08:56 -0500

    create houses routing wrapper

--- frontend/src/components/paint/Houses/index.js
+++ frontend/src/components/paint/Houses/index.js
@@ -1,58 +1,6 @@
-import React, { lazy, Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-import LoadingLock from '../Loading/LoadingLock';
-import { getAllhouses } from '../../../store/houses';
-import { setModal, showModal } from '../../../store/UX/modal';
-import {
-  clearPlusAction,
-  hidePlus,
-  setPlusAction,
-  showPlus
-} from '../../../store/UX/plus';
-
-import './houses.css';
-
-const SlidingHouseContainer = lazy(() => import('./SlidingHouseContainer'));
-
-export default function Houses ({ creating, joining, selected }) {
-  const dispatch = useDispatch();
-
-  const user = useSelector(state => state.session.user);
-  const sessionLoaded = useSelector(state => state.session.loaded);
-  const housesLoaded = useSelector(state => state.houses.allLoaded);
-
-  useEffect(() => {
-    if (sessionLoaded && user) dispatch(getAllhouses());
-  }, [dispatch, sessionLoaded, user]);
-
-  useEffect(() => {
-    const popNewHouse = () => {
-      dispatch(setModal(lazy(() => import('../NewHouse'))));
-      dispatch(showModal());
-    };
-    dispatch(setPlusAction(popNewHouse));
-    dispatch(showPlus());
-    return () => {
-      dispatch(hidePlus());
-      dispatch(clearPlusAction());
-    };
-  }, [dispatch]);
-
-  if (sessionLoaded && !user) {
-    return <Navigate to='/login' />;
-  }
-
-  return (
-    <>
-      {housesLoaded
-        ? (
-          <Suspense fallback={<LoadingLock name='residences sliding container' />}>
-            <SlidingHouseContainer selected={selected} />
-          </Suspense>
-          )
-        : <LoadingLock name='houses list fetch' />}
-    </>
-  );
+export default function Houses () {
+  return <Outlet />;
 }
