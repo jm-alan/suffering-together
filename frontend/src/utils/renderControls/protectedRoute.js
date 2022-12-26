a9536458af3a0bdfe3dc499e847b89f290ed49c6
JM <j.maxwell.alan@gmail.com>
2022-12-26 02:03:51 -0500

    protect -> protectedRoute

--- frontend/src/utils/renderControls/protectedRoute.js
+++ frontend/src/utils/renderControls/protectedRoute.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
 
-import Rebound from '../components/logic/Rebound';
-import LoadingLock from '../components/paint/Loading/LoadingLock';
+import Rebound from '../../components/logic/Rebound';
+import LoadingLock from '../../components/paint/Loading/LoadingLock';
 
-export default function protect (path, elements) {
+export default function protect (path, elements, subroutes = []) {
   const user = useSelector(state => state.session.user);
   const loaded = useSelector(state => state.session.loaded);
 
   return (
     <Route
+      key={path}
       path={path}
       element={loaded
         ? user
           ? elements
           : <Rebound interimURL='/login' finalURL={path} />
         : <LoadingLock name={`${path} pending session restore`} />}
-    />
+    >
+      {subroutes}
+    </Route>
   );
 }
